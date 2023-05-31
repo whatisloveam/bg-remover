9133d77ac246e40289b6f1f0f84cef501a57ffbc
David <96037054+DavidChili34@users.noreply.github.com>
2023-05-31 19:05:38 +0500

    edit

--- test.py
+++ test.py
@@ -1,4 +1,25 @@
 from os.path import exists
+import pytest
+from fastapi.testclient import TestClient
 
-def test_abc():
-    print(1)
+
+client = TestClient(app)
+model_path = './ckpt/u2net.pth'
+
+#import numpy as np
+
+
+def test_read():
+    response = client.post("/change-bg")
+    assert response.status_code == 200
+    assert response.json() ==  JSONResponse(
+            status_code=200,
+            content={
+                'img_with_bk': output_image,
+                'img_no_bk': img_no_bk,})
+
+
+def test_right_path():
+    assert exists('/webapp/images-input')
+    assert exists('/webapp/images-output')
+
