135511b7efccd66660ab7423380d2e25040ecb69
David <96037054+DavidChili34@users.noreply.github.com>
2023-05-31 19:06:20 +0500

    edit

--- test.py
+++ test.py
@@ -12,12 +12,7 @@ model_path = './ckpt/u2net.pth'
 def test_read():
     response = client.post("/change-bg")
     assert response.status_code == 200
-    assert response.json() ==  JSONResponse(
-            status_code=200,
-            content={
-                'img_with_bk': output_image,
-                'img_no_bk': img_no_bk,})
-
+   
 
 def test_right_path():
     assert exists('/webapp/images-input')
